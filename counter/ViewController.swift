ee907dd4dd12ead8318ee249a2c2f3397ab21658
Arslan Mushtaq <m.arslanmushtaqahmed@gmail.com>
2020-08-21 21:27:19 +0500

    add error check for negative values

--- counter/ViewController.swift
+++ counter/ViewController.swift
@@ -11,8 +11,10 @@ import UIKit
 class ViewController: UIViewController {
     
     var count: Int = 0
+    var errorFound: Bool = false
 
     @IBOutlet weak var counter: UILabel!
+    @IBOutlet weak var error: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,11 +22,20 @@ class ViewController: UIViewController {
     }
 
     @IBAction func increaseCounter(_ sender: UIButton) {
+        if (errorFound) {
+            errorFound = false
+            error.text = ""
+        }
         count = count + 1
         counter.text = "\(count)"
     }
     
     @IBAction func decreaseCounter(_ sender: UIButton) {
+        if (count == 0) {
+            errorFound = true
+            error.text = "Negative count restricted"
+            return
+        }
         count = count - 1
         counter.text = "\(count)"
     }
